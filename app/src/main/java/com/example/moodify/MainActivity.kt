b3033fc2b376de0271e29781c4f267793ce5d6f5
Emma Meline <emma.meline@edu.ece.fr>
2023-11-28 16:50:06 +0900

    fragments for all pages except the add an entry (i have a issue with the first icon which is selectet at lauch)

--- app/src/main/java/com/example/moodify/MainActivity.kt
+++ app/src/main/java/com/example/moodify/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.moodify
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageButton
+import androidx.fragment.app.Fragment
 import com.example.moodify.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 //google dependencies
 
@@ -24,6 +27,45 @@ class MainActivity : AppCompatActivity() {
         //make the item placeholder (to align icons in bottomBar) disabled
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
+        //BottomBar which display different fragment depends on user's action
+        val bottomNavigationView: BottomNavigationView = binding.bottomNavigationView
 
+        bottomNavigationView.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.meditate ->{
+                    loadFragment(Meditate())
+                    true
+                }
+                R.id.reaction ->{
+                    loadFragment(addMood())
+                    true
+                }
+                R.id.call ->{
+                    loadFragment(Call())
+                    true
+                }
+                R.id.profile ->{
+                    loadFragment(Profile())
+                    true
+                }
+                else -> false
+
+            }
+
+
+        }
+
+
+        bottomNavigationView.selectedItemId = -1
+
+    }
+
+    //function to display fragments
+    private  fun loadFragment(fragment: Fragment){
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(binding.mainFrag.id,fragment)
+        transaction.commit()
     }
 }
