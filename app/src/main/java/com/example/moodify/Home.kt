f7ee3f47764af3a22871baf40e9a928266becb17
jaa7522 <jaa7522@thi.de>
2023-12-15 13:27:02 +0900

    Display next three appointments in home fragment

--- app/src/main/java/com/example/moodify/Home.kt
+++ app/src/main/java/com/example/moodify/Home.kt
@@ -1,37 +1,61 @@
 package com.example.moodify
 
+import android.Manifest
+import android.accounts.AccountManager
+import android.app.Activity
+import android.app.ProgressDialog
+import android.content.Context
+import android.content.Intent
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.moodify.databinding.FragmentAddMoodBinding
-import com.example.moodify.databinding.FragmentCallBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.moodify.databinding.FragmentGetEventBinding
 import com.example.moodify.databinding.FragmentHomeBinding
+import com.example.moodify.model.GetEventModel
+import com.example.moodify.util.Constants.PREF_ACCOUNT_NAME
+import com.example.moodify.util.Constants.REQUEST_ACCOUNT_PICKER
+import com.example.moodify.util.Constants.REQUEST_AUTHORIZATION
+import com.example.moodify.util.Constants.REQUEST_GOOGLE_PLAY_SERVICES
+import com.example.moodify.util.Constants.REQUEST_PERMISSION_GET_ACCOUNTS
+import com.example.moodify.util.executeAsyncTask
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.api.client.extensions.android.http.AndroidHttp
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.client.util.DateTime
+import com.google.api.client.util.ExponentialBackOff
+import com.google.api.services.calendar.Calendar
+import com.google.api.services.calendar.CalendarScopes
+import com.google.api.services.calendar.model.Event
+import com.google.api.services.calendar.model.EventDateTime
+import kotlinx.coroutines.cancel
+import pub.devrel.easypermissions.EasyPermissions
+import java.util.Date
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Home.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Home : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-    lateinit var binding: FragmentHomeBinding
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    private var mCredential: GoogleAccountCredential? = null
+    private var mService: Calendar? = null
+
+    var mProgress: ProgressDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        Log.d("ITM", "coucou je suis là")
+        initCredentials()
     }
 
     override fun onCreateView(
@@ -39,28 +63,227 @@ class Home : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
-        Log.d("ITM", "coucou je suis là")
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        initView()
+
         return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Home().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            REQUEST_GOOGLE_PLAY_SERVICES -> if (resultCode != Activity.RESULT_OK) {
+                Log.d("Google Calendar", "This app requires Google Play Services. Please install " + "Google Play Services on your device and relaunch this app.")
+            } else {
+                getResultsFromApi()
+            }
+            REQUEST_ACCOUNT_PICKER -> if (resultCode == Activity.RESULT_OK && data != null &&
+                data.extras != null
+            ) {
+                val accountName = data.getStringExtra(AccountManager.KEY_ACCOUNT_NAME)
+                if (accountName != null) {
+                    val settings = this.activity?.getPreferences(Context.MODE_PRIVATE)
+                    val editor = settings?.edit()
+                    editor?.putString(PREF_ACCOUNT_NAME, accountName)
+                    editor?.apply()
+                    mCredential!!.selectedAccountName = accountName
+                    getResultsFromApi()
                 }
             }
+            REQUEST_AUTHORIZATION -> if (resultCode == Activity.RESULT_OK) {
+                getResultsFromApi()
+            }
+        }
+    }
+
+    private fun initView() {
+        mProgress = ProgressDialog(requireContext())
+        mProgress!!.setMessage("Loading...")
+        getResultsFromApi()
+    }
+
+    fun getDataFromCalendar(): MutableList<GetEventModel> {
+        val now = DateTime(System.currentTimeMillis())
+        val eventStrings = ArrayList<GetEventModel>()
+        try {
+            val events = mService!!.events().list("primary")
+                .setMaxResults(3)
+                .setTimeMin(now)
+                .setOrderBy("startTime")
+                .setSingleEvents(true)
+                .execute()
+            val items = events.items
+
+            for (event in items) {
+                var start = event.start.dateTime
+                if (start == null) {
+                    start = event.start.date
+                }
+
+                eventStrings.add(
+                    GetEventModel(
+                        summary = event.summary,
+                        startDate = start.toString()
+                    )
+                )
+            }
+            return eventStrings
+
+        } catch (e:UserRecoverableAuthIOException) {
+            Log.d("Google", e.cause.toString())
+            startActivityForResult(e.intent, REQUEST_AUTHORIZATION)
+        }
+        return eventStrings
+    }
+
+    private fun initCredentials() {
+        mCredential = GoogleAccountCredential.usingOAuth2(
+            requireContext(),
+            arrayListOf(CalendarScopes.CALENDAR)
+        )
+            .setBackOff(ExponentialBackOff())
+        initCalendarBuild(mCredential)
+    }
+
+    private fun initCalendarBuild(credential: GoogleAccountCredential?) {
+        val transport = AndroidHttp.newCompatibleTransport()
+        val jsonFactory = JacksonFactory.getDefaultInstance()
+        mService = Calendar.Builder(
+            transport, jsonFactory, credential
+        )
+            .setApplicationName("GetEventCalendar")
+            .build()
+    }
+
+    private fun getResultsFromApi() {
+        if (!isGooglePlayServicesAvailable()) {
+            acquireGooglePlayServices()
+        } else if (mCredential!!.selectedAccountName == null) {
+            chooseAccount()
+            Log.d("Google Calendar", "Choose account!")
+        } else if (!isDeviceOnline()) {
+            Log.d("ITM", "No network connection available.")
+        } else {
+            makeRequestTask()
+        }
+    }
+
+    private fun chooseAccount() {
+        if (EasyPermissions.hasPermissions(requireContext(), Manifest.permission.GET_ACCOUNTS)) {
+            val accountName = null
+            this.activity?.getPreferences(Context.MODE_PRIVATE)?.getString(PREF_ACCOUNT_NAME, null)
+            if (accountName != null) {
+                mCredential!!.selectedAccountName = accountName
+                getResultsFromApi()
+            } else {
+                // Start a dialog from which the user can choose an account
+                startActivityForResult(
+                    mCredential!!.newChooseAccountIntent(),
+                    REQUEST_ACCOUNT_PICKER
+                )
+            }
+        } else {
+            // Request the GET_ACCOUNTS permission via a user dialog
+            EasyPermissions.requestPermissions(
+                this,
+                "This app needs to access your Google account (via Contacts).",
+                REQUEST_PERMISSION_GET_ACCOUNTS,
+                Manifest.permission.GET_ACCOUNTS
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    //Google consolea erişim izni olup olmadıgına bakıyoruz
+    private fun isGooglePlayServicesAvailable(): Boolean {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(requireContext())
+        return connectionStatusCode == ConnectionResult.SUCCESS
+    }
+
+    private fun acquireGooglePlayServices() {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val connectionStatusCode = apiAvailability.isGooglePlayServicesAvailable(requireContext())
+        if (apiAvailability.isUserResolvableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode)
+        }
+    }
+
+    fun showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode: Int) {
+        val apiAvailability = GoogleApiAvailability.getInstance()
+        val dialog = apiAvailability.getErrorDialog(
+            this,
+            connectionStatusCode,
+            REQUEST_GOOGLE_PLAY_SERVICES
+        )
+        dialog?.show()
+    }
+
+    private fun isDeviceOnline(): Boolean {
+        val connMgr =
+            this.activity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connMgr.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+
+    private fun makeRequestTask() {
+        var mLastError: Exception? = null
+
+        lifecycleScope.executeAsyncTask(
+            onStart = {
+                mProgress!!.show()
+            },
+            doInBackground = {
+                try {
+                    getDataFromCalendar()
+                } catch (e: Exception) {
+                    mLastError = e
+                    lifecycleScope.cancel()
+                    null
+                }
+            },
+            onPostExecute = { output ->
+                mProgress!!.hide()
+                if (output == null || output.size == 0) {
+                    Log.d("Google Calendar", "Calendar fetch: No data")
+                } else {
+                    var textOutput = ""
+                    for (index in 0 until output.size) {
+                        textOutput += output[index].summary + ": " + output[index].startDate.slice(0..9).replace('-', '/') + "\n"
+                    }
+                    binding.nextAppointments.text = "Next appointments:\n" + textOutput
+                }
+            },
+            onCancelled = {
+                mProgress!!.hide()
+                if (mLastError != null) {
+                    if (mLastError is GooglePlayServicesAvailabilityIOException) {
+                        showGooglePlayServicesAvailabilityErrorDialog(
+                            (mLastError as GooglePlayServicesAvailabilityIOException)
+                                .connectionStatusCode
+                        )
+                    } else if (mLastError is UserRecoverableAuthIOException) {
+                        this.startActivityForResult(
+                            (mLastError as UserRecoverableAuthIOException).intent,
+                            REQUEST_AUTHORIZATION
+                        )
+                    } else {
+                        Log.d("ITM", "The following error occurred while fetching the calendar:\n" + mLastError!!.message)
+                    }
+                } else {
+                    Log.d("Google Calendar", "Calendar fetch cancelled.")
+                }
+            }
+        )
     }
 }
